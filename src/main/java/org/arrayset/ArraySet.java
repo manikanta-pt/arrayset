4a77e36dbcaa8481c2763693a9fa640d4ae5d5d9
Manikanta <manikanta@egovernments.org>
2016-08-29 17:46:02 +0530

    refactored code

--- src/main/java/org/arrayset/ArraySet.java
+++ src/main/java/org/arrayset/ArraySet.java
@@ -1,49 +1,63 @@
 package org.arrayset;
 
+/**
+ * 
+ * @author mani
+ *
+ */
 
 public class ArraySet {
-    private Integer[] es;
+	private Integer[] arraySet;
 
-    public ArraySet() {
-        this.es = new Integer[0];
-    }
+	/**
+	 * Array set Constructor
+	 */
+	public ArraySet() {
+		this.arraySet = new Integer[0];
+	}
 
-    public Integer[] toArray() {
-        return this.es;
-    }
+	public Integer[] toArray() {
+		return this.arraySet;
+	}
 
-    public void addNewElemnt(Integer element) {
-        int i = 0;
-        int e = 0;
-        if (this.es.length == 0) {
-            if (element == null) {
-                throw new NullPointerException();
-            }
-            this.es = new Integer[1];
-            this.es[0] = element;
-            return;
-        }
-        for (i = 0; i < this.es.length; i++) {
-            if (this.es[i].equals(element)) {
-                e = 1;
-            } else {
-                continue;
-            }
-        }
-        if (e == 0) {
-            Integer[] anotherElements = new Integer[this.es.length + 1];
-            for (i = 0; i < this.es.length; i++) {
-                anotherElements[i] = this.es[i];
-            }
-            if (element == null) {
-                throw new NullPointerException();
-            } else {
-                anotherElements[this.es.length] = element;
-                this.es = anotherElements;
-            }
+	public boolean addNewElemnt(Integer element) {
+		
+		boolean exists = false;
+		int length = this.arraySet.length;
+		if (length == 0) {
+			if (element == null) {
+				throw new NullPointerException();
+			}
+			this.arraySet = new Integer[1];
+			this.arraySet[0] = element;
+			return !exists;
+		}
+		if (!contains(element)) {
+			Integer[] anotherElements = new Integer[length + 1];
+			for (int counter = 0; counter < length; counter++) {
+				anotherElements[counter] = this.arraySet[counter];
+			}
+			if (element == null) {
+				throw new NullPointerException();
+			} else {
+				anotherElements[length] = element;
+				this.arraySet = anotherElements;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-        } else {
-
-        }
-    }
+	private boolean contains(Integer element) {
+		int length = this.arraySet.length;
+		for (int counter = 0; counter < length; counter++) {
+			if (this.arraySet[counter].equals(element)) {
+				return true;
+			} else {
+				continue;
+			}
+		}
+		return false;
+	}
 }
